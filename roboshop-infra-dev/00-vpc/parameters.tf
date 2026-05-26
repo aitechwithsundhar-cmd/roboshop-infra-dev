fd89ac9fb04639ef7263b955c595fbe4c2ed529c
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-05-26 22:48:17 +0700

    Added VPC outputs

--- roboshop-infra-dev/00-vpc/parameters.tf
+++ roboshop-infra-dev/00-vpc/parameters.tf
@@ -1,4 +1,4 @@
-resource "aws_ssmparameter" "vpc_id" {
+resource "aws_ssm_parameter" "vpc_id" {
   name  = "/${var.project}/${var.environment}/vpc_id"
   type  = "String"
   value = module.vpc.vpc_id
