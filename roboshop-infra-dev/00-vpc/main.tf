11309b8e43265430d2d0c433a09a2e296be8a688
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-05-26 12:19:00 +0700

    action

--- roboshop-infra-dev/00-vpc/main.tf
+++ roboshop-infra-dev/00-vpc/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source      = "git::https://github.com/aitechwithsundhar-cmd/terraform-aws-vpc.git?ref=main"
-  project     = "roboshop"
-  environment = "dev"
+  project     = var.project
+  environment = var.environment
   is_peering_required = true
 }
