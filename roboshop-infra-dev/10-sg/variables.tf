418f1eea51f0e1874a485e3b91a12839db41f347
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-05-27 20:18:14 +0700

    variable action

--- roboshop-infra-dev/10-sg/variables.tf
+++ roboshop-infra-dev/10-sg/variables.tf
@@ -5,3 +5,21 @@ variable "project" {
 variable "environment" {
   default = "dev"
 }
+
+variable "sg_names"{
+  type = list(string)
+  default = [
+    #database security group
+    "mongodb", "mysql", "redis", "rabbitmq",
+    #backend security group
+    "catalogue", "cart", "user", "shipping", "payment",
+    #backend ALB
+    "backend_alb",
+    #frontend 
+    "frontend",
+    #frontend ALB
+    "frontend_alb",
+    #bastion host
+    "bastion"
+    ]
+}
