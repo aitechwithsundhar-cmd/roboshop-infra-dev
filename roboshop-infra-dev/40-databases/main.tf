e2e192dffea3d2856b3a3e18d14be0877ce83ad7
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-05-29 22:45:19 +0700

    terraform

--- roboshop-infra-dev/40-databases/main.tf
+++ roboshop-infra-dev/40-databases/main.tf
@@ -19,7 +19,7 @@ resource "terraform_data" "bootstrap" {
     connection {
         type        = "ssh"
         user        = "ec2-user"
-        password    = "DevOps123"
+        password    = "DevOps321"
         host        = aws_instance.mongodb.private_ip
     }
     provisioner "file" {
