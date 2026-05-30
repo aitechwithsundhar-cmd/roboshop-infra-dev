f6746142743c25d4aceb3865a02a11ed24c1822e
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-05-30 13:27:24 +0700

    ansible roles terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+component=$1
+
 dnf install ansible -y 
+
+git clone https://github.com/aitechwithsundhar-cmd/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf
+ansible-playbook -e component=$component roboshop.yaml
--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -10,3 +10,4 @@ locals {
   mongodb_sg_id      = data.aws_ssm_parameter.mongodb_sg_id.value
 
 }
+
