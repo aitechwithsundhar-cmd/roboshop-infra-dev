1051658bfe817ad2e48564bfa20d583262c9bc30
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-05-30 18:52:58 +0700

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -35,3 +35,30 @@ resource "aws_security_group_rule" "mongodb_user" {
   source_security_group_id = local.user_sg_id
   security_group_id        = local.mongodb_sg_id
 }
+
+resource "aws_security_group_rule" "redis_bastion" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = local.bastion_sg_id
+  security_group_id        = local.redis_sg_id
+}
+
+# resource "aws_security_group_rule" "redis_catalogue" {
+#   type                     = "ingress"
+#   from_port                = 6379
+#   to_port                  = 6379
+#   protocol                 = "tcp"
+#   source_security_group_id = local.catalogue_sg_id
+#   security_group_id        = local.redis_sg_id
+# }
+
+# resource "aws_security_group_rule" "redis_user" {
+#   type                     = "ingress"
+#   from_port                = 6379
+#   to_port                  = 6379
+#   protocol                 = "tcp"
+#   source_security_group_id = local.user_sg_id
+#   security_group_id        = local.redis_sg_id
+# }
