7d299cbc676b0400065887de580d16f7f614688f
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-06-03 00:25:30 +0700

    terraform

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -1,10 +1,10 @@
-locals{
-    backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
-    private_subnet_ids = split(",", data.aws_ssm_parameter.subnet_ids.value)
-    common_tags = {
-        Project     = var.project
-        Environment = var.environment
-        terraform   = "true"
-    }
+locals {
+  backend_alb_sg_id  = data.aws_ssm_parameter.backend_alb_sg_id.value
+  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  common_tags = {
+    Project     = var.project
+    Environment = var.environment
+    terraform   = "true"
+  }
 }
 
--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -4,10 +4,10 @@ resource "aws_lb" "backend_alb" {
   load_balancer_type = "application"
   security_groups    = [local.backend_alb_sg_id]
   subnets            = local.private_subnet_ids
-#keep it as false/ just to delete using terrafrom while practice 
-enable_deletion_protection = false
+  #keep it as false/ just to delete using terrafrom while practice 
+  enable_deletion_protection = false
 
-tags = merge(
+  tags = merge(
     {
       Name = "${var.project}-${var.environment}"
     },
@@ -15,17 +15,17 @@ tags = merge(
   )
 }
 
-  resource "aws_lb_listener" "hhtp" {
-    load_balancer_arn = aws_lb.backend_alb.arn
-    port              = 80
-    protocol          = "HTTP"
-    default_action {
-        type = "fixed-response"
-        fixed_response {
-            content_type = "text/html"
-            message_body = "<h1>hi, iam frrom HTTP backend ALB</h1>"
-            status_code  = "200"
-        }
+resource "aws_lb_listener" "hhtp" {
+  load_balancer_arn = aws_lb.backend_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1>hi, iam frrom HTTP backend ALB</h1>"
+      status_code  = "200"
+    }
   }
 }
 
@@ -33,7 +33,7 @@ resource "aws_route53_record" "www" {
   zone_id = var.zone_id
   name    = "*.backend-${var.environment}.${var.domain_name}"
   type    = "A"
-  
+
   #load_balancer details
   alias {
     name                   = aws_lb.backend_alb.dns_name
