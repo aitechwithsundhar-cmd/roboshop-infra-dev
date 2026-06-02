7a119edde1e2c1a726e0fcfb4665a58ce94d8c11
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-06-03 00:09:45 +0700

    terraform

---
+++ 50-backend-alb/main.tf
@@ -0,0 +1,43 @@
+resource "aws_lb" "backend_alb" {
+  name               = "${var.project}-${var.environment}"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [local.backend_alb_sg_id]
+  subnets            = local.private_subnet_ids
+}
+#keep it as false/ just to delete using terrafrom while practice 
+enable_deletion_protection = false
+
+tags = merge(
+    {
+      Name = "${var.project}-${var.environment}"
+    },
+    local.common_tags
+  )
+
+  resource "aws_lb_listener" "hhtp" {
+    load_balancer_arn = aws_lb.backend_alb.arn
+    port              = 80
+    protocol          = "HTTP"
+    default_action {
+        type = "fixed-response"
+        fixed_response {
+            content_type = "text/html"
+            message_body = "<h1>hi, iam frrom HTTP backend ALB</h1>"
+            status_code  = "200"
+        }
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = "*.backend-${var.environment}.${var.domain_name}"
+  type    = "A"
+  
+  #load_balancer details
+  alias {
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+}
