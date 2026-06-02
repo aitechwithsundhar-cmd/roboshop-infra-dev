e24fab71437f0520f3c53aaa6a167ee8d470a0d4
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-06-02 21:55:36 +0700

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -7,7 +7,9 @@ dnf install ansible -y
 cd /home/ec2-user
 git clone https://github.com/aitechwithsundhar-cmd/ansible-roboshop-roles-tf.git
 
+
 cd ansible-roboshop-roles-tf
+git pull
 ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
 
 
