71887b46757af95243ad2553784a77292b2cc2a4
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-06-02 21:02:08 +0700

    terraform

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -60,26 +60,3 @@ resource "aws_iam_instance_profile" "bastion" {
   role = aws_iam_role.bastion.name
 }
 
-resource "aws_iam_role" "mysql" {
-  name = local.mysql_role_name
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  tags = merge(
-    {
-      name = local.mysql_role_name
-    },
-    local.common_tags
-  )
-}
