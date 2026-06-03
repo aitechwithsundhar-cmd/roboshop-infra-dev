ce948fbd43cb75dfccaef6371a2a0101dd490a3a
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-06-03 22:09:14 +0700

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -69,6 +69,6 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   to_port                  = 80
   protocol                 = "tcp"
   source_security_group_id = local.bastion_sg_id
-  security_group_id        = local.backend_albsg_id
+  security_group_id        = local.backend_alb_sg_id
 }
 
