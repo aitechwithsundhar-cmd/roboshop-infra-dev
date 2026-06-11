4598defbd99863906fd610074cf56bc800e0b7cb
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-06-12 03:38:23 +0700

    action

--- 80-frontend-alb/provider.tf
+++ 80-frontend-alb/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket       = "remote-state-techai-dev"
-    key          = "roboshop-dev-backend-alb"
+    key          = "roboshop-dev-frontend-alb"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
