e9e5398556191f13e1ccb6498f6b5c4f22139a8d
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-06-12 03:15:45 +0700

    action

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+variable "zone_id" {
+  default = "Z00261213KEKMBRHYD2W"
+
+}
+
+variable "domain_name" {
+  default = "techdaws.online"
+}
