c9c191bcd1e7e07fefc4a001e1256c70c42885c2
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-06-12 04:21:57 +0700

    action

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -81,20 +81,20 @@ resource "aws_security_group_rule" "catalogue_bastion" {
   security_group_id        = local.catalogue_sg_id
 }
 
-# resource "aws_security_group_rule" "catalogue_backend_alb" {
-#   type                     = "ingress"
-#   from_port                = 8080
-#   to_port                  = 8080
-#   protocol                 = "tcp"
-#   source_security_group_id = local.backend_alb_sg_id
-#   security_group_id        = local.user_sg_id
-# }
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  source_security_group_id = local.backend_alb_sg_id
+  security_group_id        = local.user_sg_id
+}
 
-# resource "aws_security_group_rule" "frontend_alb_public" {
-#   type                     = "ingress"
-#   from_port                = 443
-#   to_port                  = 443
-#   protocol                 = "tcp"
-#   cidr_blocks              = ["0.0.0.0/0"]
-#   security_group_id        = local.frontend_alb_sg_id
-# }
+resource "aws_security_group_rule" "frontend_alb_public" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  cidr_blocks              = ["0.0.0.0/0"]
+  security_group_id        = local.frontend_alb_sg_id
+}
