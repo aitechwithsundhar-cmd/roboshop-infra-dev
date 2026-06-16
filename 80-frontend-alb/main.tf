bb2965a76639ea21da179be619a7091bb37c9023
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-06-17 05:36:51 +0700

    action

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -15,22 +15,22 @@ resource "aws_lb" "frontend_alb" {
   )
 }
 
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.frontend_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = local.frontend_alb_certificate_arn
+# resource "aws_lb_listener" "https" {
+#   load_balancer_arn = aws_lb.frontend_alb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = local.frontend_alb_certificate_arn
 
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/html"
-      message_body = "<h1>hi, iam frrom HTTPS frontend ALB</h1>"
-      status_code  = "200"
-    }
-  }
-}
+#   default_action {
+#     type = "fixed-response"
+#     fixed_response {
+#       content_type = "text/html"
+#       message_body = "<h1>hi, iam frrom HTTPS frontend ALB</h1>"
+#       status_code  = "200"
+#     }
+#   }
+# }
 
 resource "aws_route53_record" "www" {
   zone_id = var.zone_id
