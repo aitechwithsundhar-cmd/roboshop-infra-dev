f37105ab7945e54b5f3e579dd0a2a3968da05605
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-06-17 01:36:19 +0700

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,7 +1,7 @@
 module "component" {
-    for_each = var.components
-    source = "git::https://github.com/aitechwithsundhar-cmd/terraform-roboshop-component.git?ref=main"
-    component = each.key
-    rule_priority = each.value_priority
+  for_each      = var.components
+  source        = "git::https://github.com/aitechwithsundhar-cmd/terraform-roboshop-component.git?ref=main"
+  component     = each.key
+  rule_priority = each.value.rule_priority
 
-    }
+}
