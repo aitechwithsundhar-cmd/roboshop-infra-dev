0f0df64514494a7872d6c8649c6498efaaed79a8
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-07-01 22:06:21 +0700

    action

---
+++ 95-cdn/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  cachingDisabled  = data.aws_cloudfront_cache_policy.cachingDisabled.id
+  cachingOptimized = data.aws_cloudfront_cache_policy.cachingOptimized.id
+  acm_certificate_arn = data.aws_ssm_parameter.acm_certificate_arn.value
+}
--- 95-cdn/provider.tf
+++ 95-cdn/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket       = "remote-state-techai-dev"
-    key          = "roboshop-dev-components"
+    key          = "roboshop-dev-cdn"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
